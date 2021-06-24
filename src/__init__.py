f081615397b775eb84b7163f72ebe625389fb987
aguijarro <ec.andres.guijarro@gmail.com>
2021-06-23 21:30:16 -0500

    ADD: project config environment file

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,9 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+# set config
+app.config.from_object('src.config.DevelopmentConfig')
+
 
 class PayRoll(Resource):
     def get(self):
